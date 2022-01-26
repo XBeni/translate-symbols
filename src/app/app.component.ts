552408803aa1dec3aa79a866ab3e48d05decb104
natanael.cioarba <natanael.cioarba@atm-maggioli.es>
2022-01-26 16:50:39 +0100

    translate-symbols

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'translate-symbols';
+  sTexto: string = '';
+  sTextoConvertido: string = '';
+
+  replaceText() {
+    const chars:any = {
+      'Ă':'A',
+      'ă':'a',
+      'Â':'A',
+      'â':'a',
+      'Î':'I',
+      'î':'i',
+      'Ș':'S',
+      'ș':'s',
+      'Ț':'T',
+      'ț':'t'
+    };
+    this.sTextoConvertido = this.sTexto.replace(/[ĂăÂâÎîȘșȚț]/g, m => chars[m]);
+  }
 }
